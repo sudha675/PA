d6eef2c652724f89867e50976ba52dad53126a91
sudha675 <sudharaju434@gmail.com>
2025-07-10 15:40:18 +0530

    update the data

--- Problem2.py
+++ Problem2.py
@@ -1,15 +1,48 @@
+# Import libraries
 import nltk
-from nltk.stem import PorterStemmer
+from nltk.stem import PorterStemmer, WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+from nltk.corpus import wordnet
 
-input="i am still learing python and i am enjoying it"
+# Download necessary data
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 
-word=word_tokenize(input)
+# -------------------------------
+# STEMMING
+# -------------------------------
+input_text = "i am still learing python and i am enjoying it"
+word_tokens = word_tokenize(input_text)
 
-ps=PorterStemmer()
+ps = PorterStemmer()
+stemmed_words = [ps.stem(token) for token in word_tokens]
 
-stemmer_word=[]
-for token in word:
-    stemmer_word.append(ps.stem(token))
+print("Stemmer Words:", stemmed_words)
 
-print(stemmer_word)
+# -------------------------------
+# LEMMATIZATION with POS tagging
+# -------------------------------
+lemm_text = "I am enjoying learning and running with my friends."
+lemm_tokens = word_tokenize(lemm_text)
+lemm_pos_tags = pos_tag(lemm_tokens)
+
+# Function to convert POS tags to WordNet format
+def get_wordnet_pos(tag):
+    if tag.startswith('J'):
+        return wordnet.ADJ
+    elif tag.startswith('V'):
+        return wordnet.VERB
+    elif tag.startswith('N'):
+        return wordnet.NOUN
+    elif tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN  # default to noun
+
+lemmatizer = WordNetLemmatizer()
+lemmatized_words = [lemmatizer.lemmatize(token, get_wordnet_pos(pos)) for token, pos in lemm_pos_tags]
+
+print("Lemmatizer Words:", lemmatized_words)
