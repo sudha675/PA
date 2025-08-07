8295f7ef926565c10235ef79359d2ae6e92bf914
sudha675 <sudharaju434@gmail.com>
2025-08-07 13:58:19 +0530

    update

---
+++ Problem9.py
@@ -0,0 +1,31 @@
+#Program 11: SVM
+
+from sklearn import datasets
+iris = datasets.load_iris()
+
+x = iris.data
+y = iris.target
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y,train_size=0.8)
+
+from sklearn import svm
+svc_cl = svm.SVC(probability=True)
+
+train = svc_cl.fit(x_train, y_train)
+y_pred = svc_cl.predict(x_test)
+print(y_pred)
+y_auc = svc_cl.predict_proba(x_test)
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+Accuracy = accuracy_score(y_test, y_pred)
+Precision = precision_score(y_test, y_pred, average='macro')
+Recall = recall_score(y_test, y_pred, average='macro')
+f1 = f1_score(y_test, y_pred, average='macro')
+auc = roc_auc_score(y_test, y_auc, multi_class='ovr')
+
+print("\nAccuracy : ", Accuracy)
+print("Precision : ", Precision)
+print("Recall : ", Recall)
+print("F1-SCORE : ", f1)
+print("AUC : ", auc)
